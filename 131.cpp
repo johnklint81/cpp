c79373fcd8da4c16319f63296cf61a4d2561d5cd
John Klint <gusjohn25@student.gu.se>
2024-11-21 18:20:33 +0100

    cpp

--- 131.cpp
+++ 131.cpp
@@ -4,29 +4,39 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    int number = stoi(argv[1]);
-    int remainder;
+
+stack<int> getBinary(int number) {
     // We store on a stack because this method gives us the binary digits in
     // reversed order (least significant to most significant). We then print
     // by popping from the stack.
-    stack<int> binaries;
+    stack<int> binary;
+
     // Base case
     if (number == 0) {
-        cout << "0" << endl;
-        return 0;
+        binary.push(0);
+        return binary;
     }
+
     // Divide by 2, the remainder is the constant in front of that binary 
     // position. It is either 0 or 1. Push to stack.
     while (number != 0) {
-        remainder = number % 2;
+        int remainder = number % 2;
+        binary.push(remainder);
         number /= 2;
-        binaries.push(remainder);
     }
-    while (!binaries.empty()) {
+    return binary;
+}
+
+
+int main(int argc, char *argv[]) {
+    int number = stoi(argv[1]);
+    int remainder;
+    stack<int> binary = getBinary(number);
+
+    while (!binary.empty()) {
         // Unfortunately pop() does not return item like in Python
-        cout << binaries.top();
-        binaries.pop();
+        cout << binary.top();
+        binary.pop();
     }
     cout << endl;
 }
