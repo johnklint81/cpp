b3819ba83d1b446f80ae03040096bf9268d18c09
John Klint <gusjohn25@student.gu.se>
2024-11-24 21:18:14 +0100

    211

---
+++ 211.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int size = 100;
+    // Note, can NOT be const int, because vector needs to be able to modify
+    // elements!
+    vector<int> numbers(size);
+    // iota works like np.arange() except args are:
+    // (iterator pointing to the position in the container where the sequence
+    // will begin, iterator pointing to the end, value to begin iterating from).
+    // The reason that iterators are used and not just raw pointers is to 
+    // provide an abstraction that works consistently across all types of
+    // containers (arrays, maps, lists, etc.
+    iota(numbers.begin(), numbers.end(), 0);
+    float average = static_cast<float>(accumulate(numbers.begin(),
+                                                  numbers.end(),
+                                                  0)) / numbers.size();
+    // min_element and max_element returns pointers to iterators so they need
+    // to be dereferenced
+    const int minElement = *min_element(numbers.begin(), numbers.end());
+    const int maxElement = *max_element(numbers.begin(), numbers.end());
+    cout << "The elements in the vector are:" << endl;
+    for_each(numbers.begin(), numbers.end(), [](int num) {
+        cout << num << " ";
+    });
+    cout << endl;
+    cout << "Mean:" << average << endl;
+    cout << "Max: " << maxElement << endl;
+    cout << "Min: " << minElement << endl;
+}
